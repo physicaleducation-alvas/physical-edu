4a9155643f03795d4a3e795a739a414215a6daba
prajwalachandake22-pixel <prajwalachandake22@gmail.com>
2025-12-10 12:27:41 +0530

    Update: Minor changes in about page

---
+++ src/app/about/page.js
@@ -0,0 +1,18 @@
+export default function AEFDescription() {
+  return (
+    <div className="p-6 leading-relaxed text-justify">
+        <div className="p-5"></div>
+
+      {/* Headline */}
+      <h1 className="text-3xl font-bold text-red-600 mb-4">
+        About
+      </h1>
+
+      {/* Paragraph */}
+      <p>
+        Alva’s Education Foundation is a multifaceted organization created by Dr. M. Mohan Alva, considered as the crown of Moodbidri. It has 20 educational institutions with more than 20,000 students. It is a trust registered under the Trust Act with an objective of catering to the need of cultural and educational development. It started its activity in the year 1995 with Nursing and Ayurveda streams of education. Now it has primary to Post Graduate courses in General Education, Para Medical Courses, Engineering and Management, and Alternative Medicine, encompassing students from all over India and abroad.
+      </p>
+
+    </div>
+  );
+}
