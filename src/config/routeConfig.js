edf816cd02247dc9fae155c005bb6d2ae0e5c956
praneethk000 <praneethkpoojary000@gmail.com>
2025-12-17 10:06:51 +0530

    Update: Added Mohan Alva image

--- src/config/routeConfig.js
+++ src/config/routeConfig.js
@@ -19,7 +19,7 @@ export const navRoutes = [
         path: '/about/chairman-message',
         id: 'chairman-message',
         type: 'rich-card',
-        image: '/images/chairman.png',
+        image: '/images/mohan-alva.jpg',
         name: 'Dr. Mohan Alva',
         qualification: `Master's`,
         description: 'Our chairman shares our long-term vision and values.',
