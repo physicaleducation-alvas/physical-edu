2ff7a301a642a30f4f1f6e177f04b5b5fccfe80b
praneethk000 <praneethkpoojary000@gmail.com>
2025-12-24 11:59:19 +0530

    Update: start of activities page

---
+++ src/app/activities/layout.js
@@ -0,0 +1,10 @@
+export default function activitiesLayout({ children }) {
+    return (
+        <>
+            <div className="p-10">
+
+            </div>
+            {children}
+        </>
+    )
+}
