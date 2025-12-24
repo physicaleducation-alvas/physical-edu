6ff63d6fc325d7c0a158f30e7aa6716efc4cc39d
praneethk000 <praneethkpoojary000@gmail.com>
2025-12-24 12:30:28 +0530

    Update:minor updates

--- src/app/college/placements/page.js
+++ src/app/college/placements/page.js
@@ -1,7 +1,11 @@
 export default function placementsPage() {
     return (
         <>
-            <div className="p-10">Placements Page</div>
+            <div className="p-10 mt-7">
+                <h1 className="subtitle1 text-2xl mb-5">Placements</h1>
+                <h4 className="subtitle1">Alva's Pragathi</h4>
+                <div className="content2 text-justify">A job recruitment drive called alvas pragathi is conducted annually by alvas education foundatition , around 300 companies come for campus recruitment for various disciplines including physical education.</div>
+            </div>
         </>
     )
 }
--- src/app/college/teaching/page.js
+++ src/app/college/teaching/page.js
@@ -1,7 +1,10 @@
 export default function teachingPage() {
     return (
         <>
-            <div className="p-10">Teaching Page</div>
+            <div className="p-10 mt-7">
+                <h1 className="subtitle1 text-2xl mb-5">Teaching Methodology</h1>
+                <div className="content2 text-justify">The teaching facility consists of highly qualified full time staff with the adjunct staff of part time instructor for various sports and games and guest faculty of highly placed professionals. Audio visual training aids are available to supplement class room sessions. Yoga classes are arranged to give a practical insight. Seminars and workshops are conducted regularly to enhance the teaching ability of fellow students. Qualified NIS coaches are also invited to take special classes for higher performances and to improve coaching qualities of B.P.Ed students. Importance is given for improving Communication skills of the students by conducting communicative English classes.</div>
+            </div>
         </>
     )
 }
